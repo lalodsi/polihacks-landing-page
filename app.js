99283d2776fad5a8161d9fe8bea273be77620ef6
Luis Rodriguez <lalo_luis_6@hotmail.com>
2021-12-01 15:31:01 -0600

    Arreglado enlace para cambio de idioma

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const lang = navigator.language
 console.log(lang)
 
 if((lang.toLocaleLowerCase().includes('es')) && (window.location.toString().toLocaleLowerCase().includes('index_es.html')==false)){
-    window.location="file:///root/Escritorio/polihacks_landing/polihacks-landing-page/index_es.html"
+    window.location="file:///./index_es.html"
 }
