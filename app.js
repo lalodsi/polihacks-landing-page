0acd18aa89251543ae93cf9379cd78b0510c00e6
Luis Rodriguez <lalo_luis_6@hotmail.com>
2021-12-03 23:29:16 -0600

    language change activated

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const lang = navigator.language
 console.log(lang)
 
-// if((lang.toLocaleLowerCase().includes('es')) && (window.location.toString().toLocaleLowerCase().includes('index_es.html')==false)){
-//     window.location="./index_es.html"
-// }
+if((lang.toLocaleLowerCase().includes('es')) && (window.location.toString().toLocaleLowerCase().includes('index_es.html')==false)){
+    window.location="./index_es.html"
+}
