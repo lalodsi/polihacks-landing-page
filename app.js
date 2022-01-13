f0b17e6c4de35cb9e2c7f7ed567b901f27d647c4
Luis Rodriguez <lalo_luis_6@hotmail.com>
2022-01-13 12:33:38 -0600

    code for changing language

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
-const lang = navigator.language
+cambiarIdioma('español', 'english')
+let lang = navigator.language
 console.log(lang)
 
 // if((lang.toLocaleLowerCase().includes('es')) && (window.location.toString().toLocaleLowerCase().includes('index_es.html')==false)){
 //     window.location="./index_es.html"
 // }
+
+const langButton = document.getElementById('language-button');
+
+langButton.addEventListener('click', () =>{
+    if (lang == 'es-419') {
+        console.log('Changing to english');
+        lang = 'en'
+        cambiarIdioma('español', 'english')
+    }
+    else{
+        console.log('Cambiando a español');
+        lang = navigator.language
+        cambiarIdioma('english', 'español')
+    }
+})
+
+function cambiarIdioma(languageHidden, languageShowed){
+    const Idioma1 = document.querySelectorAll(`[lang="${languageHidden}"]`);
+    const Idioma2 = document.querySelectorAll(`[lang="${languageShowed}"]`);
+    Idioma1.forEach( element => {
+        element.setAttribute('style', 'display: none;')
+    } )
+    Idioma2.forEach( element => {
+        element.setAttribute('style', 'display: inline;')
+        // element.style.visibility = 'visible';
+    } )
+}
